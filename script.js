6051b54e0e77de467c270d74be22f1d55df49576
lojain <lojainnasr039@gmail.com>
2024-01-03 09:46:27 +0300

    update

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function search(event){
 event.preventDefault();
-
+let searchInput = document.querySelector("#search");
+let cityElement = document.querySelector("#city");
+cityElement.innerHTML = `${searchInput.value}`;
 let apiKey = "ec49etfea43b9bf6o7c77b0d49c77081";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key=${apiKey}`;
+let city =  searchInput.value;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
 axios.get(apiUrl).then(checkweather);
 }
-
+let form = document.querySelector("#form");
+form.addEventListener('click', search);
